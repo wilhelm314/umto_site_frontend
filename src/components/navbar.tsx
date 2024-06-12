644ffdb229119b62cdfb393a3a0b81354caea8f0
wilhelm <wilhelm.tranberg.rasmussen@outlook.dk>
2024-06-12 18:43:20 +0200

    style changes

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,13 +7,13 @@ export function navbar() {
 
 
     return (
-        <div className="container bg-grey mx-auto max-w-full p-5 font-Consolas text-xl font-semibold ">
-            <div className="mx-10 max-w-full text-right	tracking-wide antialiased">
+        <div className="container bg-grey mx-auto max-w-full p-5 font-Consolas text-2xl font-bold ">
+            <div className="mx-10 max-w-full text-right	tracking-wider antialiased">
 
-                <a href="" className="m-10 text-black hover:text-maincolor">HOME</a>
-                <a href="" className="m-10 text-black hover:text-maincolor">PROJECTS</a>
-                <a href="" className="m-10 text-black hover:text-maincolor">ABOUT</a>
-                <a href="" className="m-10 text-black hover:text-maincolor">MAP</a>
+                <a href="" className="m-20 text-black hover:text-maincolor">HOME</a>
+                <a href="" className="m-20 text-black hover:text-maincolor">PROJECTS</a>
+                <a href="" className="m-20 text-black hover:text-maincolor">ABOUT</a>
+                <a href="" className="m-20 text-black hover:text-maincolor">MAP</a>
             </div>
 
         </div>
